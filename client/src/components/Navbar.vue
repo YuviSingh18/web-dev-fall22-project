b431a4f5eee99cc8bc4c395b4e406746e840441d
Yuvi <yuvrajsinghji18@gmail.com>
2022-10-17 13:14:21 -0400

    some changes

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="https://bulma.io/">
+                <img src="https://bulma.io/images/bulma-logo-white.png" width="112" height="28">
+            </a>
+        </div>
+      
+        <div class="navbar-menu">
+            <div class="navbar-start">
+                <a class="navbar-item" href="https://bulma.io/">
+                Home
+                </a>
+        
+                <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link" href="https://bulma.io/documentation/overview/start/">
+                        Docs
+                    </a>
+            
+                    <div class="navbar-dropdown">
+                        <a class="navbar-item" href="https://bulma.io/documentation/overview/start/">
+                        Overview
+                        </a>
+                        <a class="navbar-item" href="https://bulma.io/documentation/overview/modifiers/">
+                        Modifiers
+                        </a>
+                        <a class="navbar-item" href="https://bulma.io/documentation/columns/basics/">
+                        Columns
+                        </a>
+                        <a class="navbar-item" href="https://bulma.io/documentation/layout/container/">
+                            Layout
+                        </a>
+                        <a class="navbar-item" href="https://bulma.io/documentation/form/general/">
+                            Forms
+                        </a>
+                        <hr class="navbar-divider">
+                        <a class="navbar-item" href="https://bulma.io/documentation/elements/box/">
+                        Elements
+                        </a>
+                        <a class="navbar-item is-active" href="https://bulma.io/documentation/components/breadcrumb/">
+                            Components
+                        </a>
+                    </div>
+                </div>
+            </div>
+      
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <a class="button is-light">
+                            <span class="icon"><i class="fab fa-twitter"></i></span>
+                            <strong>Tweet</strong>
+                        </a>
+                        <a class="button is-primary">
+                            <span class="icon"><i class="fa fa-download"></i></span>
+                            <strong>Download</strong>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+
+
+<style scoped>
+
+</style>
