75807fca59f2938513fb3e61671fee95deab26d9
Yuvi <yuvrajsinghji18@gmail.com>
2022-12-01 14:54:59 -0500

    debugging

--- server/models/mongo.js
+++ server/models/mongo.js
@@ -3,7 +3,6 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const uri = process.env.MONGO_URI;
 
-
 const client = new MongoClient(uri, { serverApi: { version: ServerApiVersion.v1 } });
 module.exports = {
     connect: () => client.connect()
